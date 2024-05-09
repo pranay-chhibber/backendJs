290d3f4f13507a211da22adc33b23287d6a3d79d
Pranay Chhibber <pranaychhibber111@gmail.com>
2024-05-09 23:15:49 +0530

    adding routes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,7 +6,7 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret:process.env.CLOUDINARY_API_SECRET 
 });
-const uploadOnCloudinart = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
         //@ uploading file on clouduinary
@@ -27,4 +27,4 @@ cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/
 
 
 
-  export {uploadOnCloudinart}
+  export {uploadOnCloudinary}
